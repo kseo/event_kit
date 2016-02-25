50262d00a1d6d6731a51fd92da1b5299f7e3fab7
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-02-25 15:39:56 +0900

    Don't put library name in test scripts.

--- test/event_kit_test.dart
+++ test/event_kit_test.dart
@@ -1,8 +1,6 @@
 // Copyright (c) 2015, Kwang Yul Seo. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library event_kit.test;
-
 import 'package:event_kit/event_kit.dart';
 import 'package:test/test.dart';
 
